e5b9d184ae2385bd291b4f7c01ea6d6ec71f8a1d
jet86 <tim@timjones.id.au>
2024-10-18 16:20:42 +1100

    Add psutil for storage, memory, and swap

--- coreplugins/diagnostic/plugin.py
+++ coreplugins/diagnostic/plugin.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext as _
 
-import json, shutil
+import json, shutil, psutil
 
-def get_memory_stats():
+def get_memory_stats_old():
     """
     Get node total memory and memory usage (Linux only)
     https://stackoverflow.com/questions/17718449/determine-free-ram-in-python
@@ -38,22 +38,41 @@ class Plugin(PluginBase):
     def app_mount_points(self):
         @login_required
         def diagnostic(request):
+            # Disk space old
+            total_disk_space_old, used_disk_space_old, free_disk_space_old = shutil.disk_usage('./')
+            
             # Disk space
-            total_disk_space, used_disk_space, free_disk_space = shutil.disk_usage('./')
+            disk_usage = psutil.disk_usage('./')
+            
+            # Memory
+            memory_usage = psutil.virtual_memory()
+            swap_usage = psutil.swap_memory()
 
             template_args = {
                 'title': 'Diagnostic',
-                'total_disk_space': total_disk_space,
-                'used_disk_space': used_disk_space,
-                'free_disk_space': free_disk_space
+                # Disk space old
+                'total_disk_space_old': total_disk_space_old,
+                'used_disk_space_old': used_disk_space_old,
+                'free_disk_space_old': free_disk_space_old,
+                # Disk space
+                'total_disk_space': disk_usage.total,
+                'used_disk_space': disk_usage.used,
+                'free_disk_space': disk_usage.free,
+                # Memory
+                'total_memory': memory_usage.total,
+                'used_memory': memory_usage.total - memory_usage.available,
+                'free_memory': memory_usage.available,
+                'total_swap': swap_usage.total,
+                'used_swap': swap_usage.used,
+                'free_swap': swap_usage.free
             }
 
             # Memory (Linux only)
-            memory_stats = get_memory_stats()
-            if 'free' in memory_stats:
-                template_args['free_memory'] = memory_stats['free']
-                template_args['used_memory'] = memory_stats['used']
-                template_args['total_memory'] = memory_stats['total']
+            memory_stats_old = get_memory_stats_old()
+            if 'free' in memory_stats_old:
+                template_args['free_memory_old'] = memory_stats_old['free']
+                template_args['used_memory_old'] = memory_stats_old['used']
+                template_args['total_memory_old'] = memory_stats_old['total']
 
             return render(request, self.template_path("diagnostic.html"), template_args)
 
